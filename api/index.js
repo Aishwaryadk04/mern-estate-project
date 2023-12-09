225dd5c442198fe6210d2303ebc936e3305695b5
Aishwaryadk04 <aishwaryadkofficial@gmail.com>
2023-12-09 15:50:06 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,15 +1,15 @@
 import express from 'express';
-// import mongoose from 'mongoose';
-// import dotenv from 'dotenv';
-// dotenv.config();
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-// mongoose
-//   .connect(process.env.MONGO)
-//   .then(()=>{
-//     console.log('connected to MongoDB');
-//   }).catch((err)=>{
-//     console.log(err);
-//   })
+mongoose
+  .connect(process.env.MONGO)
+  .then(()=>{
+    console.log('connected to MongoDB');
+  }).catch((err)=>{
+    console.log(err);
+  })
 
 const app = express();
 
