211f9a9b397e7fde2fd05cd4a667b154e64798b3
Aishwaryadk04 <aishwaryadkofficial@gmail.com>
2023-12-12 17:28:44 +0530

    Update the header and make profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -29,12 +29,13 @@ export default function Header() {
                 <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
             </Link>
 
-            <Link to={'/sign-in'} > 
-            {currentUser? (
-                <img src={currentUser.avatar} alt="profile" />
-            ):    (       <li className=' sm:inline text-slate-700 hover:underline'>Sign In</li>
+            <Link to='/profile'>
+            {currentUser ? (
+              <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt='profile' />
+            ) : (
+              <li className=' text-slate-700 hover:underline'> Sign in</li>
             )}
-             </Link>
+          </Link>
 
         </ul>
     </div>
