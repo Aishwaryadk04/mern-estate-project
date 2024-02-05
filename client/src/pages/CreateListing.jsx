76193b22440b181ec2f121145f84ccc89735f8ef
Aishwaryadk04 <aishwaryadkofficial@gmail.com>
2024-02-05 12:00:46 +0530

    final

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -285,7 +285,7 @@ export default function CreateListing() {
               <div className='flex flex-col items-center'>
                 <p>Regular price</p>
                 {formData.type === 'rent' && (
-                  <span className='text-xs'>($ / month)</span>
+                  <span className='text-xs'>(₹ / month)</span>
                 )}
               </div>
             </div>
@@ -304,7 +304,7 @@ export default function CreateListing() {
                 <div className='flex flex-col items-center'>
                   <p>Discounted price</p>
                   {formData.type === 'rent' && (
-                    <span className='text-xs'>($ / month)</span>
+                    <span className='text-xs'>(₹ / month)</span>
                   )}
                 </div>
               </div>
